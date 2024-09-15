4b2abd92379439ee10fd588a53121e570b8730e7
NishkalHundia <nishkalnhundia@gmail.com>
2024-09-15 05:56:10 -0400

    AGENTS WORK

--- run_agents.py
+++ run_agents.py
@@ -3,7 +3,7 @@ from uagents import Model
 from uagents.query import query
 from uagents.envelope import Envelope
 import asyncio
-from custom_input_agent import PDFInputResponse
+# from custom_input_agent import PDFInputResponse
 import json
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ AGENT_ADDRESS = "agent1qd9m7gwn5ankrvlvdqkxyf6mnsxd6yvhf3xgrj97gv0fjjavj263qt890
 # Define data model for incoming requests
 class PDFProcessRequest(Model):
     pdf_path: str
-    image_dir: str 
+    image_dir: str
 
 class TestResponse(Model):
     response: str
@@ -25,13 +25,27 @@ async def process_pdf():
     pdf_path = request_data['pdf']
     img_path = request_data['img']
 
+    pdf_path="coinchanging.pdf"
+    img_path="img_dir"
+
     print(f"PDF Path: {pdf_path}")
     print(f"Image Path: {img_path}")
 
     response = await query(destination = AGENT_ADDRESS, message = PDFProcessRequest(pdf_path = pdf_path, image_dir = img_path),timeout = 15.0)
+    print(response)
+    if isinstance(response, Envelope):
+        data = json.loads(response.decode_payload())
+        return data
 
-    response = json.loads(response.decode_payload())
-    return response
+    else:
+        return "not an instance"
+
+    # response = json.loads(response.decode_payload())
+    # return data
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
